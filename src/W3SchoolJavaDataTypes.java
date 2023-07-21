af3e9ebbbebcbe5c79cbf505d5edf5538eac06d4
rmeirosu <rmeirosu@deloittece.com>
2023-07-21 15:58:12 +0300

    Completed W3School Java Data Types exercises.

---
+++ src/W3SchoolJavaDataTypes.java
@@ -0,0 +1,31 @@
+public class W3SchoolJavaDataTypes {
+    public static void main(String[] args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+    }
+
+    public void exercise1() {
+        //Add the correct data type for the following variables:
+        int myNum = 9;
+        float myFloatNum = 8.99F;
+        char myLetter = 'A';
+        boolean myBool = false;
+        String myText = "Hello World";
+    }
+
+    public void exercise2() {
+        /*
+        byte, short, int, long, float, double, boolean and char are called:
+        Primitive data types.
+         */
+    }
+
+    public void exercise3() {
+        //Type casting - convert the following double type (myDouble) to an int type:
+        double myDouble = 9.78D;
+        int myInt = (int) myDouble;
+    }
+}
