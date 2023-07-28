d2b37c7204a03f4bd287f5de524983d1b3415563
rmeirosu <rmeirosu@deloittece.com>
2023-07-28 11:53:49 +0300

    Added CodingBat website String-1 exercises up until seeColor.

--- src/CodingBatStrings.java
+++ src/CodingBatStrings.java
@@ -30,11 +30,158 @@ public class CodingBatStrings {
         System.out.println(makeTags("cite", "Yay"));
         System.out.println("============");
 
+        // makeOutWord
+        System.out.println("CodingBat String-1 exercise: makeOutWord");
+        System.out.println(makeOutWord("<<>>", "Yay"));
+        System.out.println(makeOutWord("<<>>", "WooHoo"));
+        System.out.println(makeOutWord("[[]]", "word"));
+        System.out.println("============");
+
+        // extraEnd
+        System.out.println("CodingBat String-1 exercise: extraEnd");
+        System.out.println(extraEnd("Hello"));
+        System.out.println(extraEnd("ab"));
+        System.out.println(extraEnd("Hi"));
+        System.out.println("============");
+
+        // firstTwo
+        System.out.println("CodingBat String-1 exercise: firstTwo");
+        System.out.println(firstTwo("Hello"));
+        System.out.println(firstTwo("abcdefg"));
+        System.out.println(firstTwo("ab"));
+        System.out.println("============");
+
+        // firstHalf
+        System.out.println("CodingBat String-1 exercise: firstHalf");
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
+        System.out.println("============");
+
+        // withoutEnd
+        System.out.println("CodingBat String-1 exercise: withoutEnd");
+        System.out.println(withoutEnd("Hello"));
+        System.out.println(withoutEnd("java"));
+        System.out.println(withoutEnd("coding"));
+        System.out.println("============");
+
+        // comboString
+        System.out.println("CodingBat String-1 exercise: comboString");
+        System.out.println(comboString("Hello", "hi"));
+        System.out.println(comboString("hi", "Hello"));
+        System.out.println(comboString("aaa", "b"));
+        System.out.println("============");
+
+        // nonStart
+        System.out.println("CodingBat String-1 exercise: nonStart");
+        System.out.println(nonStart("Hello", "There"));
+        System.out.println(nonStart("java", "code"));
+        System.out.println(nonStart("shotl", "java"));
+        System.out.println("============");
+
+        // left2
+        System.out.println("CodingBat String-1 exercise: left2");
+        System.out.println(left2("Hello"));
+        System.out.println(left2("java"));
+        System.out.println(left2("Hi"));
+        System.out.println("============");
+
+        // right2
+        System.out.println("CodingBat String-1 exercise: right2");
+        System.out.println(right2("Hello"));
+        System.out.println(right2("java"));
+        System.out.println(right2("Hi"));
+        System.out.println("============");
+
+        // theEnd
+        System.out.println("CodingBat String-1 exercise: theEnd");
+        System.out.println(theEnd("Hello", true));
+        System.out.println(theEnd("Hello", false));
+        System.out.println(theEnd("oh", true));
+        System.out.println("============");
+
+        // withouEnd2
+        System.out.println("CodingBat String-1 exercise: withouEnd2");
+        System.out.println(withouEnd2("Hello"));
+        System.out.println(withouEnd2("abc"));
+        System.out.println(withouEnd2("ab"));
+        System.out.println("============");
+
+        // middleTwo
+        System.out.println("CodingBat String-1 exercise: middleTwo");
+        System.out.println(middleTwo("string"));
+        System.out.println(middleTwo("code"));
+        System.out.println(middleTwo("Practice"));
+        System.out.println("============");
+
+        // endsLy
+        System.out.println("CodingBat String-1 exercise: endsLy");
+        System.out.println(endsLy("oddly"));
+        System.out.println(endsLy("y"));
+        System.out.println(endsLy("oddy"));
+        System.out.println("============");
+
+        // nTwice
+        System.out.println("CodingBat String-1 exercise: nTwice");
+        System.out.println(nTwice("Hello", 2));
+        System.out.println(nTwice("Chocolate", 3));
+        System.out.println(nTwice("Chocolate", 1));
+        System.out.println("============");
+
+        // twoChar
+        System.out.println("CodingBat String-1 exercise: twoChar");
+        System.out.println(twoChar("java", 0));
+        System.out.println(twoChar("java", 2));
+        System.out.println(twoChar("java", 3));
+        System.out.println("============");
+
+        // middleThree
+        System.out.println("CodingBat String-1 exercise: middleThree");
+        System.out.println(middleThree("Candy"));
+        System.out.println(middleThree("and"));
+        System.out.println(middleThree("solving"));
+        System.out.println("============");
+
+        // hasBad
+        System.out.println("CodingBat String-1 exercise: hasBad");
+        System.out.println(hasBad("badxx"));
+        System.out.println(hasBad("xbadxx"));
+        System.out.println(hasBad("xxbadxx"));
+        System.out.println("============");
+
+        // atFirst
+        System.out.println("CodingBat String-1 exercise: atFirst");
+        System.out.println(atFirst("hello"));
+        System.out.println(atFirst("hi"));
+        System.out.println(atFirst("h"));
+        System.out.println("============");
+
+        // lastChars
+        System.out.println("CodingBat String-1 exercise: lastChars");
+        System.out.println(lastChars("last", "chars"));
+        System.out.println(lastChars("yo", "java"));
+        System.out.println(lastChars("hi", ""));
+        System.out.println("============");
+
+        // conCat
+        System.out.println("CodingBat String-1 exercise: conCat");
+        System.out.println(conCat("abc", "cat"));
+        System.out.println(conCat("dog", "cat"));
+        System.out.println(conCat("abc", ""));
+        System.out.println("============");
+
+        // lastTwo
+        System.out.println("CodingBat String-1 exercise: lastTwo");
+        System.out.println(lastTwo("coding"));
+        System.out.println(lastTwo("cat"));
+        System.out.println(lastTwo("ab"));
+        System.out.println("============");
+
 
     }
 
     public static String helloName(String name) {
-    // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+        // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
         return "Hello " + name + "!";
     }
 
@@ -53,6 +200,369 @@ public class CodingBatStrings {
     Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
      */
         return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    public static String makeOutWord(String out, String word) {
+    /*
+    Given an "out" string length 4, such as "<<>>", and a word, return a new string where
+    the word is in the middle of the out string, e.g. "<<word>>".
+    Note: use str.substring(i, j) to extract the String starting at index i
+    and going up to but not including index j.
+
+    makeOutWord("<<>>", "Yay") → "<<Yay>>"
+    makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+    makeOutWord("[[]]", "word") → "[[word]]"
+     */
+        return (out.substring(0, 2) + word + out.substring(2, 4));
+    }
+
+    public static String extraEnd(String str) {
+    /*
+    Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+    The string length will be at least 2.
+
+    extraEnd("Hello") → "lololo"
+    extraEnd("ab") → "ababab"
+    extraEnd("Hi") → "HiHiHi"
+     */
+        return (str.substring(str.length() - 2)) + (str.substring(str.length() - 2)) + (str.substring(str.length() - 2));
+    }
+
+    public static String firstTwo(String str) {
+    /*
+    Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+    If the string is shorter than length 2, return whatever there is, so "X" yields "X",
+    and the empty string "" yields the empty string "".
+    Note that str.length() returns the length of a string.
+
+    firstTwo("Hello") → "He"
+    firstTwo("abcdefg") → "ab"
+    firstTwo("ab") → "ab"
+     */
+        if (str.length() >= 2) {
+            return (str.substring(0, 2));
+
+        } else {
+            return str;
+        }
+    }
+
+    public static String firstHalf(String str) {
+    /*
+    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+    firstHalf("WooHoo") → "Woo"
+    firstHalf("HelloThere") → "Hello"
+    firstHalf("abcdef") → "abc"
+     */
+        return (str.substring(0, str.length() / 2));
+    }
+
+    public static String withoutEnd(String str) {
+    /*
+    Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+    withoutEnd("Hello") → "ell"
+    withoutEnd("java") → "av"
+    withoutEnd("coding") → "odin"
+     */
+        return (str.substring(1, str.length() - 1));
+    }
+
+    public static String comboString(String a, String b) {
+    /*
+    Given 2 strings, a and b, return a string of the form short+long+short,
+    with the shorter string on the outside and the longer string on the inside.
+    The strings will not be the same length, but they may be empty (length 0).
+
+    comboString("Hello", "hi") → "hiHellohi"
+    comboString("hi", "Hello") → "hiHellohi"
+    comboString("aaa", "b") → "baaab"
+     */
+
+        if (a.length() < b.length()) {
+            return a + b + a;
+        } else {
+            return b + a + b;
+        }
+    }
+
+    public static String nonStart(String a, String b) {
+    /*
+    Given 2 strings, return their concatenation, except omit the first char of each.
+    The strings will be at least length 1.
+
+    nonStart("Hello", "There") → "ellohere"
+    nonStart("java", "code") → "avaode"
+    nonStart("shotl", "java") → "hotlava"
+     */
+
+        return (a.substring(1, a.length()) + b.substring(1, b.length()));
+    }
+
+    public static String left2(String str) {
+    /*
+    Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+    The string length will be at least 2.
+
+    left2("Hello") → "lloHe"
+    left2("java") → "vaja"
+    left2("Hi") → "Hi"
+     */
+
+        return (str.substring(2) + str.substring(0, 2));
+    }
+
+    public static String right2(String str) {
+
+    /*
+    Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+    The string length will be at least 2.
+
+    right2("Hello") → "loHel"
+    right2("java") → "vaja"
+    right2("Hi") → "Hi"
+     */
+
+        return (str.substring(str.length() - 2, str.length())
+                + str.substring(0, str.length() - 2));
+
+    }
+
+    public static String theEnd(String str, boolean front) {
+    /*
+    Given a string, return a string length 1 from its front, unless front is false,
+    in which case return a string length 1 from its back. The string will be non-empty.
+
+    theEnd("Hello", true) → "H"
+    theEnd("Hello", false) → "o"
+    theEnd("oh", true) → "o"
+     */
+
+        if (front == true) {
+            return (str.substring(0, 1));
+        } else {
+            return (str.substring(str.length() - 1));
+        }
+    }
+
+    public static String withouEnd2(String str) {
+    /*
+    Given a string, return a version without both the first and last char of the string.
+    The string may be any length, including 0.
+
+    withouEnd2("Hello") → "ell"
+    withouEnd2("abc") → "b"
+    withouEnd2("ab") → ""
+     */
+        if (str.length() >= 2) {
+            return (str.substring(1, str.length() - 1));
+        } else {
+            return "";
+        }
+    }
+
+    public static String middleTwo(String str) {
+    /*
+    Given a string of even length, return a string made of the middle two chars,
+    so the string "string" yields "ri". The string length will be at least 2.
+
+    middleTwo("string") → "ri"
+    middleTwo("code") → "od"
+    middleTwo("Practice") → "ct"
+     */
+
+        return str.substring(str.length() / 2 - 1, str.length() / 2 + 1);
+
+    }
+
+    public static boolean endsLy(String str) {
+    /*
+    Given a string, return true if it ends in "ly".
+
+    endsLy("oddly") → true
+    endsLy("y") → false
+    endsLy("oddy") → false
+     */
+
+        if (str.length() < 2) {
+            return false;
+        } else if (str.substring(str.length() - 2, str.length()).equals("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String nTwice(String str, int n) {
+    /*
+    Given a string and an int n, return a string made of the first and last n chars from the string.
+    The string length will be at least n.
+
+    nTwice("Hello", 2) → "Helo"
+    nTwice("Chocolate", 3) → "Choate"
+    nTwice("Chocolate", 1) → "Ce"
+     */
+
+        if (n > 0) {
+            return (str.substring(0, n) + str.substring(str.length() - n));
+        } else {
+            return "";
+        }
+
+    }
+
+    public static String twoChar(String str, int index) {
+    /*
+    Given a string and an index, return a string length 2 starting at the given index.
+    If the index is too big or too small to define a string length 2, use the first 2 chars.
+    The string length will be at least 2.
+
+    twoChar("java", 0) → "ja"
+    twoChar("java", 2) → "va"
+    twoChar("java", 3) → "ja"
+     */
+
+        if (index < 0 || index > (str.length() - 2)) {
+            return str.substring(0,2);
+        } else {
+            return str.substring(index, index + 2);
+        }
+
+    }
+
+    public static String middleThree(String str) {
+    /*
+    Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and".
+    The string length will be at least 3.
+
+    middleThree("Candy") → "and"
+    middleThree("and") → "and"
+    middleThree("solving") → "lvi"
+     */
+
+        if (str.length() %2 != 0 && str.length() > 2) { // %2 == 0 means even number (no remainder)
+            return str.substring(str.length()/2-1,str.length()/2+2);
+        } else {
+            return "String is even number";
+        }
+
+    }
+
+    public static boolean hasBad(String str) {
+    /*
+    Given a string, return true if "bad" appears starting at index 0 or 1 in the string,
+    such as with "badxxx" or "xbadxx" but not "xxbadxx".
+    The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+
+    hasBad("badxx") → true
+    hasBad("xbadxx") → true
+    hasBad("xxbadxx") → false
+     */
+
+        if ((str.length() < 3)) {
+            return false;
+
+        } else if (str.length() == 3 && !(str.substring(0,1).equals("b"))) {
+            return false;
+
+        } else if (str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad")) {
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+
+    public static String atFirst(String str) {
+    /*
+    Given a string, return a string length 2 made of its first 2 chars.
+    If the string length is less than 2, use '@' for the missing chars.
+
+    atFirst("hello") → "he"
+    atFirst("hi") → "hi"
+    atFirst("h") → "h@"
+     */
+
+        if (str.length() == 0) {
+            return "@@";
+
+        } else if (str.length() < 2) {
+            return (str + "@");
+
+        } else {
+            return (str.substring(0,2));
+        }
+    }
+
+    public static String lastChars(String a, String b) {
+    /*
+    Given 2 strings, a and b, return a new string made of the first char of a and the last char of b,
+    so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+    lastChars("last", "chars") → "ls"
+    lastChars("yo", "java") → "ya"
+    lastChars("hi", "") → "h@"
+     */
+        if (a.length() == 0 && b.length() == 0) {
+            return "@@";
+
+        } else if (a.length() == 0) {
+            return ("@" + b.substring(b.length()-1,b.length()));
+
+        } else if (b.length() == 0) {
+            return (a.substring(0,1) + "@");
+
+        } else {
+            return (a.substring(0,1) + b.substring(b.length()-1,b.length()));
+        }
+    }
+
+    public static String conCat(String a, String b) {
+    /*
+    Given two strings, append them together (known as "concatenation") and return the result.
+    However, if the concatenation creates a double-char, then omit one of the chars,
+    so "abc" and "cat" yields "abcat".
+
+    conCat("abc", "cat") → "abcat"
+    conCat("dog", "cat") → "dogcat"
+    conCat("abc", "") → "abc"
+     */
+
+        if (a.length() == 0) {
+            return b;
+
+        } else if (b.length() == 0) {
+            return a;
+
+        } else if (a.substring(a.length()-1,a.length()).equals(b.substring(0,1))) {
+            return a + b.substring(1,b.length());
+
+        } else  {
+            return a + b;
+        }
+
+    }
+
+    public static String lastTwo(String str) {
+    /*
+    Given a string of any length, return a new string where the last 2 chars,
+    if present, are swapped, so "coding" yields "codign".
+
+    lastTwo("coding") → "codign"
+    lastTwo("cat") → "cta"
+    lastTwo("ab") → "ba"
+     */
+
+        if (str.length() >= 2) {
+            return str.substring(0,str.length()-2)
+                    + str.substring(str.length()-1, str.length())
+                    + str.substring(str.length()-2,str.length()-1);
+
+        } else {
+            return str;
+        }
     }
 
 }
