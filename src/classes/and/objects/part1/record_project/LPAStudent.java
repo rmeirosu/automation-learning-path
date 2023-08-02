8d6508b1d67d401386c4eaa8e07372f1598003e0
rmeirosu <rmeirosu@deloittece.com>
2023-08-02 16:03:33 +0300

    Added exercises for POJO and Record section of the course

---
+++ src/classes/and/objects/part1/record_project/LPAStudent.java
@@ -0,0 +1,4 @@
+package classes.and.objects.part1.record_project;
+
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+}
---
+++ src/classes/and/objects/part1/record_project/Student.java
@@ -0,0 +1,59 @@
+package classes.and.objects.part1.record_project;
+
+public class Student {
+
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private String classList;
+
+    public Student(String id, String name, String dateOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.classList = classList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", classList='" + classList + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getClassList() {
+        return classList;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setClassList(String classList) {
+        this.classList = classList;
+    }
+
+}
