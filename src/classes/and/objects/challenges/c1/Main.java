492267e8934220a83510814d32a0105913596fd8
rmeirosu <rmeirosu@deloittece.com>
2023-08-02 09:20:56 +0300

    Added 'Simple Calculator' and 'Person' challenges.

--- src/classes/and/objects/challenges/c1/Main.java
+++ src/classes/and/objects/challenges/c1/Main.java
@@ -49,6 +49,35 @@ public class Main {
         accountOne.withdrawFunds(9293.54);
         System.out.println();
 
+        // Simple Calculator challenge
+        System.out.println("===Simple Calculator Challenge===");
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+        System.out.println();
+
+        // Person challenge
+        System.out.println("===Person Challenge===");
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println();
+
 
     }
 
