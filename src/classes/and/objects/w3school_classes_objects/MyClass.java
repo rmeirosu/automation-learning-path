602806754484959a9cafc90554cb3b7af5f50028
rmeirosu <rmeirosu@deloittece.com>
2023-08-03 16:12:59 +0300

    Added W3School exercises.

---
+++ src/classes/and/objects/w3school_classes_objects/MyClass.java
@@ -0,0 +1,24 @@
+package classes.and.objects.w3school_classes_objects;
+
+// EXERCISE 7: Fill in the missing parts to import the java.util.Scanner class from the Java API:
+import java.util.Scanner;
+
+// EXERCISE 1: Create a class called MyClass.
+
+// EXERCISE 6: The class below should not be inherited by other classes. Add the correct modifier:
+final class MyClass {
+    // EXERCISE 3: Use myObj to access and print the value of the x attribute of MyClass.
+    int x;
+
+    // EXERCISE 4: Call myMethod on the object.
+    public void myMethod() {
+        System.out.println("Hello World");
+    }
+
+    // EXERCISE 5: Create and call a class constructor of MyClass
+    public MyClass() {
+        x = 5;
+    }
+
+
+}
