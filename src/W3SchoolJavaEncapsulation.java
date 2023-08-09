a2b8f9d7aa6dc9f34638459e875f167fbcd7f442
rmeirosu <rmeirosu@deloittece.com>
2023-08-09 11:18:16 +0300

    Finished section 8 of udemy course and W3 School Encapsulation.

---
+++ src/W3SchoolJavaEncapsulation.java
@@ -0,0 +1,39 @@
+import java.util.Set;
+
+public class W3SchoolJavaEncapsulation {
+
+/*
+Encapsulation
+The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must:
+    -declare class variables/attributes as private
+    -provide public get and set methods to access and update the value of a private
+
+Why Encapsulation?
+    -Better control of class attributes and methods
+    -Class attributes can be made read-only (if you only use the get method), or write-only (if you only use the set method)
+    -Flexible: the programmer can change one part of the code without affecting other parts
+    -Increased security of data
+ */
+
+    public static void main(String[] args) {
+
+        Person myObj = new Person();
+        myObj.setName("John"); // Set the value of the name variable to "John"
+        System.out.println(myObj.getName());
+
+    }
+
+    public static class Person {
+        private String name; // private = restricted access
+
+        // Getter
+        public String getName() {
+            return name;
+        }
+
+        // Setter
+        public void setName(String newName) {
+            this.name = newName;
+        }
+    }
+}
--- src/classes/and/objects/part2/polymorphism/Movie.java
+++ src/classes/and/objects/part2/polymorphism/Movie.java
@@ -38,6 +38,10 @@ class Adventure extends Movie {
                 "Scary Movie",
                 "Something Bad Happens");
     }
+
+    public void watchAdventure() {
+        System.out.println("Watching an Adventure!");
+    }
 }
 
 class Comedy extends Movie {
@@ -54,6 +58,10 @@ class Comedy extends Movie {
                 "Something even funnier happens",
                 "Happy Ending");
     }
+
+    public void watchComedy() {
+        System.out.println("Watching a Comedy!");
+    }
 }
 
 class ScienceFiction extends Movie {
@@ -69,5 +77,9 @@ class ScienceFiction extends Movie {
                 "Bad Aliens do Bad Stuff",
                 "Space Guys Chase Aliens",
                 "Planet Blows up");
+    }
+
+    public void watchScienceFiction() {
+        System.out.println("Watching a Science Fiction!");
     }
 }
