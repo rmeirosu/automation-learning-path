c9132c78cda6bb2052ec24a21ea7f045f5a3b43d
rmeirosu <rmeirosu@deloittece.com>
2023-08-18 16:59:09 +0300

    Section 10 lessons.

--- src/arraylists/MoreLists.java
+++ src/arraylists/MoreLists.java
@@ -1,6 +1,8 @@
 package arraylists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -24,5 +26,36 @@ public class MoreLists {
         System.out.println(groceries);
 
         System.out.println("Third item = " + groceries.get(2));
+
+        if (groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("isEmpty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
     }
 }
